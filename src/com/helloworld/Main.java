4d11374a6491f933ce62a1b2f2ce3e93f2a95952
MaxVishnevsky <vishnevsky10@gmail.com>
2016-01-21 18:10:27 +0200

    Initial commit

---
+++ src/com/helloworld/Main.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+/**
+ * Created by vishnevsky on 11.01.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
